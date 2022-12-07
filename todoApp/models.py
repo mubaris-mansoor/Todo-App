5ac718dcde218e99994562de25373dfaa1df92f1
mubaris-mansoor <mubaris.mansoor@science.christuniversity.in>
2022-12-07 16:11:55 +0530

    Update 1

--- todoApp/models.py
+++ todoApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class TodoListItem(models.Model):
     content = models.TextField()
+    name = models.TextField
     date = models.DateField(auto_now_add=True) 
     user = models.CharField(max_length=100)
 
